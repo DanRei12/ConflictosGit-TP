b419d903dc0277ceb88f07f18d5c6e944ffb1c6d
miligaliano <miligaliano@gmail.com>
2023-05-14 15:48:36 -0300

    Prueba de conflictos de edicion simultanea resulto

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const usuariosRouter = require("./routes/usuarios");
 app.use(usuariosRouter);
 app.use(articulosRouter);
 
-<<<<<<< HEAD
 console.log("Ejecutando archivo app");
-=======
-console.log("hola, se va a generar conflicto")
->>>>>>> 8e0844e98409cbe8f23bddde091adaa857ec8d90
+
+
 
